5c734c86bf3d0ee9ac23b980dbfb5ead91b86dcd
Kostas Kremizas <kremizask@gmail.com>
2018-10-06 20:52:16 +0300

    Implement functionality for fetching "real random" numbers from the web

---
+++ testing_the_waters_of_ios/RandomNumberFetcher.swift
@@ -0,0 +1,87 @@
+//
+//  RandomNumberFetcher.swift
+//  testing_the_waters_of_ios
+//
+//  Created by Kostas Kremizas on 06/10/2018.
+//  Copyright © 2018 kremizas. All rights reserved.
+//
+
+import Foundation
+
+protocol HttpClient {
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask
+}
+
+extension URLSession: HttpClient {}
+
+class RandomNumberFetcher {
+    
+    private let url = URL(string: "https://api.random.org/json-rpc/1/invoke")!
+    private let httpClient: HttpClient
+    private let apiVersion = "2.0"
+    private let apiMethod = "generateIntegers"
+    private let id = 0
+    private let apiKey = "ec06126f-4d95-4757-abcb-74eebc65fcdb"
+    
+    struct RequestBody: Codable, Equatable {
+        
+        struct Params: Codable, Equatable {
+            let apiKey: String
+            let n: Int
+            let min: Int
+            let max: Int
+            let replacement: Bool
+            let base: Int
+        }
+        
+        let jsonrpc: String
+        let method: String
+        let id: Int
+        let params: Params
+    }
+    
+    struct Response: Codable {
+        struct Result: Codable {
+            let numbers: [Int]
+        }
+        
+        let result: Result
+    }
+    
+    init(httpClient: HttpClient) {
+        self.httpClient = httpClient
+    }
+    
+    func fetchRandomNumber(min: Int, max: Int, completion: @escaping (Int?, Error?) -> Void) {
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json-rpc", forHTTPHeaderField: "Content-Type")
+        let body = RequestBody(jsonrpc: apiVersion,
+                               method: apiMethod,
+                               id: id,
+                               params: .init(apiKey: apiKey,
+                                             n: 1,
+                                             min: 1,
+                                             max: 10,
+                                             replacement: true,
+                                             base: 10))
+        
+        request.httpBody = try? JSONEncoder().encode(body)
+        
+        let dataTask = httpClient.dataTask(with: request) { (data, response, error) in
+            guard
+                let responseData = data,
+                let response = try? JSONDecoder()
+                    .decode(Response.self, from: responseData),
+                let number = response.result.numbers.first
+            else {
+                completion(nil, NSError(domain: "com.storypointscalculator", code: 1, userInfo: nil))
+                return
+            }
+            
+            completion(number, nil)
+        }
+        
+        dataTask.resume()
+    }
+}
---
+++ testing_the_waters_of_iosTests/RandomNumberFetcherTests.swift
@@ -0,0 +1,117 @@
+//
+//  RandomAPITests.swift
+//  testing_the_waters_of_iosTests
+//
+//  Created by Kostas Kremizas on 06/10/2018.
+//  Copyright © 2018 kremizas. All rights reserved.
+//
+
+import XCTest
+@testable import testing_the_waters_of_ios
+
+class HttpClientFake: HttpClient {
+    
+    class URLSessionDataTaskSpy: URLSessionDataTask {
+        var resumeCalled = false
+        
+        private let completion: (Data?, URLResponse?, Error?) -> Void
+        private let fakeResponse: Data?
+        
+        init(response: Data?, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) {
+            self.completion = completionHandler
+            self.fakeResponse = response
+        }
+        
+        override func resume() {
+            resumeCalled = true
+            completion(fakeResponse, nil, nil)
+        }
+    }
+    
+    private let response: Data?
+    
+    public init(response: Data? = nil) {
+        self.response = response
+    }
+    
+    var requests: [URLRequest] = []
+    var dummyDataTasks: [URLSessionDataTaskSpy] = []
+    
+    func dataTask(with request: URLRequest, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+        requests.append(request)
+        let task = URLSessionDataTaskSpy(response: response, completionHandler: completionHandler)
+        dummyDataTasks.append(task)
+        completionHandler(response, nil, nil)
+        return task
+    }
+}
+
+class RandomNumberFetcherTests: XCTestCase {
+    var sut: RandomNumberFetcher!
+
+    func testFetchRequestsADataTaskAndResumesIt() {
+        // Arrange
+        let httpClientSpy = HttpClientFake()
+        let sut = RandomNumberFetcher(httpClient: httpClientSpy)
+        
+        var firstRequest: URLRequest? {
+            return httpClientSpy.requests.first
+        }
+        
+        // Act
+        sut.fetchRandomNumber(min: 1, max: 10) {_,_  in }
+        
+        // Assert
+        XCTAssertEqual(httpClientSpy.requests.count, 1)
+        XCTAssertEqual(httpClientSpy.dummyDataTasks.count, 1)
+        XCTAssertEqual(httpClientSpy.dummyDataTasks.first?.resumeCalled, true)
+        XCTAssertEqual(firstRequest?.httpMethod, "POST")
+        XCTAssertEqual(firstRequest?.url?.absoluteString, "https://api.random.org/json-rpc/1/invoke")
+        XCTAssertEqual(firstRequest?.value(forHTTPHeaderField: "Content-Type"), "application/json-rpc")
+        
+        let jsonString = """
+        {
+            "jsonrpc":"2.0",
+            "method":"generateIntegers",
+            "params":{
+                "apiKey":"ec06126f-4d95-4757-abcb-74eebc65fcdb",
+                "n":1,"min":1,
+                "max":10,
+                "replacement":true,
+                "base":10
+            },
+            "id":0
+        }
+        """
+        
+        assertEqualsAfterDecoding(json: jsonString, requestBody: firstRequest?.httpBody)
+    }
+    
+    private func assertEqualsAfterDecoding(json jsonString: String, requestBody: Data?) {
+        let jsonData = jsonString.data(using: .utf8)!
+        let sut = try! JSONDecoder().decode(RandomNumberFetcher.RequestBody.self, from: jsonData)
+        let expectedValue = requestBody.flatMap { try? JSONDecoder().decode(RandomNumberFetcher.RequestBody.self, from: $0) }
+        XCTAssertEqual(sut, expectedValue)
+    }
+    
+    func testTheResponseNumberInTheCallback() {
+        
+        // Arrange
+        let response = RandomNumberFetcher.Response(result: .init(numbers: [2]))
+        let responseData = try? JSONEncoder().encode(response)
+        let httpClient = HttpClientFake(response: responseData)
+        let sut = RandomNumberFetcher(httpClient: httpClient)
+        
+        // Act
+        
+        var result: Int?
+        var responseError: Error?
+        sut.fetchRandomNumber(min: 0, max: 10) { (number, error) in
+            result = number
+            responseError = error
+        }
+        
+        XCTAssertEqual(result, 2)
+        XCTAssertNil(responseError)
+    }
+}
