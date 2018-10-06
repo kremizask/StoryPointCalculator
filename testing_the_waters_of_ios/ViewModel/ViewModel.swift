d1bd6847f1674e4766b140e86be53ddbc2198638
Kostas Kremizas <kremizask@gmail.com>
2018-10-07 00:27:51 +0300

    Refactor: extract helper method for label text calculation

--- testing_the_waters_of_ios/ViewModel/ViewModel.swift
+++ testing_the_waters_of_ios/ViewModel/ViewModel.swift
@@ -19,6 +19,7 @@ class ViewModel {
     }
     
     private let initialLabelText = NSLocalizedString("Estimates are damn hard! Right? Let us do the math for you!", comment: "Default label text")
+    private static let errorText = NSLocalizedString("🤭 It turns out we underestimated the difficulty of calculating story points! Please try again!", comment: "Error Text")
     
     public init(_ storyPointsCalculator: StoryPointsCalculatorProtocol) {
         self.storyPointsCalculator = storyPointsCalculator
@@ -26,15 +27,16 @@ class ViewModel {
     
     func calculateButtonTapped() {
         storyPointsCalculator.calculate { [weak self] (number, error) in
-            guard let number = number, error == nil else {
-                self?.labelText = NSLocalizedString("🤭 It turns out we underestimated the difficulty of calculating story points! Please try again!", comment: "Error Text")
+            guard
+                let number = number,
+                let strongSelf = self,
+                error == nil else {
+                    
+                self?.labelText = ViewModel.errorText
                 return
             }
             
-            let smallStoriesText = String.localizedStringWithFormat("This story is a definite %d pointer! I double checked!", number)
-            let bigStoriesText = String.localizedStringWithFormat("Wow! I think it's a %d! Maybe we should break this down into more stories?", number)
-            
-            self?.labelText = (number > 5) ? bigStoriesText : smallStoriesText
+            strongSelf.labelText = strongSelf.labelText(forPoints: number)
         }
     }
     
@@ -43,5 +45,13 @@ class ViewModel {
         
         // Trigger callback with
         self.labelText = initialLabelText
+    }
+    
+    // MARK: Private
+    
+    private func labelText(forPoints points: Int) -> String {
+        let smallStoriesText = String.localizedStringWithFormat("This story is a definite %d pointer! I double checked!", points)
+        let bigStoriesText = String.localizedStringWithFormat("Wow! I think it's a %d! Maybe we should break this down into more stories?", points)
+        return (points > 5) ? bigStoriesText : smallStoriesText
     }
 }
